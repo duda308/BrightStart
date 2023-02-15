7d6df4705c76ded21bf222708c49851a56d841de
duda308 <dudachecan08@gmail.com>
2023-02-15 12:08:10 +0000

    Titulo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
         </p>
-     <h1>Hello BrightStarts!</h1>
+     <h1>Olá a todos ^^ </h1>
       </header>
     </div>
   );
